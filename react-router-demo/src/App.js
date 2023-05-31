5d22c9dabaf53dc6f8dc18757f8c8bfb138fa425
Andrew <globalshow6@gmail.com>
2023-05-31 22:17:31 +0300

    React Router Tutorial: 15 - Authentication and Protected Routes

--- react-router-demo/src/App.js
+++ react-router-demo/src/App.js
@@ -13,12 +13,16 @@ import {NewProducts} from "./components/NewProducts";
 import {Users} from "./components/Users";
 import {UserDetails} from "./components/UserDetails";
 import {Admin} from "./components/Admin";
+import {Profile} from "./components/Profile";
+import {AuthProvider} from "./components/auth";
+import {Login} from "./components/Login";
+import {RequireAuth} from "./components/RequireAuth";
 
 const LazyAbout = React.lazy(() => import('./components/About'))
 
 function App() {
   return (
-      <>
+      <AuthProvider>
           <Navbar />
           <Routes>
               <Route path='/' element={<Home />} />
@@ -36,9 +40,14 @@ function App() {
                   <Route path='admin' element={<Admin />}/>
                   <Route path=':userId' element={<UserDetails />}/>
               </Route>
+              <Route path='profile' element={
+                  <RequireAuth>
+                      <Profile />
+                  </RequireAuth>} />
+              <Route path='login' element={<Login />} />
               <Route path='*' element={<NoMatch />} />
           </Routes>
-      </>
+      </AuthProvider>
   );
 }
 
