11b22a9a873da4fb211f79204e47f6f657292d0c
Andrew <globalshow6@gmail.com>
2023-05-31 19:02:56 +0300

    React Router Tutorial: 10 - Dynamic Routes

--- react-router-demo/src/App.js
+++ react-router-demo/src/App.js
@@ -9,6 +9,9 @@ import {NoMatch} from "./components/NoMatch";
 import {Products} from "./components/Products";
 import {FeaturedProducts} from "./components/FeaturedProducts";
 import {NewProducts} from "./components/NewProducts";
+import {Users} from "./components/Users";
+import {UserDetails} from "./components/UserDetails";
+import {Admin} from "./components/Admin";
 
 function App() {
   return (
@@ -22,6 +25,10 @@ function App() {
                   <Route index element={<FeaturedProducts />}/>
                   <Route path='featured' element={<FeaturedProducts />} />
                   <Route path='new' element={<NewProducts />} />
+              </Route>
+              <Route path='users' element={<Users />}>
+                  <Route path='admin' element={<Admin />}/>
+                  <Route path=':userId' element={<UserDetails />}/>
               </Route>
               <Route path='*' element={<NoMatch />} />
           </Routes>
