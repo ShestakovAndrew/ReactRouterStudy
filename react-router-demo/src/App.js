ad6fff0ae235e24c481ae37e12b6e3ef37701318
Andrew <globalshow6@gmail.com>
2023-05-31 16:37:39 +0300

    React Router Tutorial: 4 - Links

--- react-router-demo/src/App.js
+++ react-router-demo/src/App.js
@@ -2,13 +2,17 @@ import './App.css';
 import {Routes, Route} from "react-router-dom";
 import {Home} from "./components/Home";
 import {About} from "./components/About";
+import {NavigationBar} from "./components/NavigationBar";
 
 function App() {
   return (
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='about' element={<About />} />
-      </Routes>
+      <>
+          <NavigationBar />
+          <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='about' element={<About />} />
+          </Routes>
+      </>
   );
 }
 
