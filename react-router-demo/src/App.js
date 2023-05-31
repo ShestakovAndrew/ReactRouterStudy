08cc9977e9f37aaa1d00976b431a03a793238457
Andrew <globalshow6@gmail.com>
2023-05-31 17:39:00 +0300

    React Router Tutorial: 7 - No Match Route

--- react-router-demo/src/App.js
+++ react-router-demo/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import {Routes, Route} from "react-router-dom";
+
 import {Home} from "./components/Home";
 import {About} from "./components/About";
 import {Navbar} from "./components/Navbar";
 import {OrderSummary} from "./components/OrderSummary";
+import {NoMatch} from "./components/NoMatch";
 
 function App() {
   return (
@@ -13,6 +15,7 @@ function App() {
               <Route path='/' element={<Home />} />
               <Route path='about' element={<About />} />
               <Route path='order-summary' element={<OrderSummary />} />
+              <Route path='*' element={<NoMatch />} />
           </Routes>
       </>
   );
